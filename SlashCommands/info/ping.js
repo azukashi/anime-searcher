f18611302d80cfe93ca4ba2bdde783f93ca50a57
Gifaldy Azka <falcxxdev@pm.me>
2021-11-12 00:38:30 +0700

    feat: use prettier for code formatting (#5)
    * chore: install prettier
    
    * chore: add prettier scripts
    
    * chore: code format
    
    * feat: add prettier config
    
    * chore: code format
    
    * feat: add format-checking workflows
    
    * chore: code format
    
    Co-authored-by: gifaldyazkaa <falcxxdev@pm.me>

--- SlashCommands/info/ping.js
+++ SlashCommands/info/ping.js
@@ -1,16 +1,16 @@
-const { Client, CommandInteraction } = require("discord.js");
+const { Client, CommandInteraction } = require('discord.js');
 
 module.exports = {
-    name: "ping",
-    description: "Show client Ping Latency",
-    options: [],
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-    run: async (client, interaction, args) => {
-        interaction.followUp({ content: `${client.ws.ping}ms!` });
-    },
+  name: 'ping',
+  description: 'Show client Ping Latency',
+  options: [],
+  /**
+   *
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    interaction.followUp({ content: `${client.ws.ping}ms!` });
+  },
 };
